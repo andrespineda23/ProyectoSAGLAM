244068827c28320c1c0297f4f2164a354fa3c597
andrespineda23 <andres.pin51@hotmail.es>
2014-10-12 14:29:02 -0500

    Modificacion WAR

---
+++ ProyectoSAGLAM-war/src/java/Controlador/ControlLogin.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Controlador;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author ANDRES PINEDA
+ */
+@ManagedBean
+@SessionScoped
+public class ControlLogin {
+
+    /**
+     * Creates a new instance of ControlLogin
+     */
+    public ControlLogin() {
+    }
+    
+}
