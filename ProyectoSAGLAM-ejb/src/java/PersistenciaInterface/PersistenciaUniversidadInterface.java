4061f0593d4f69b5c1b30b3232025c44c273f337
andrespineda23 <andres.pin51@hotmail.es>
2014-10-12 14:24:30 -0500

    Creacion Repo

---
+++ ProyectoSAGLAM-ejb/src/java/PersistenciaInterface/PersistenciaUniversidadInterface.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PersistenciaInterface;
+
+/**
+ *
+ * @author ANDRES PINEDA
+ */
+public interface PersistenciaUniversidadInterface {
+
+    public String validarUsuarioPorDatosUniversidad(String correo, String numeroDocumento, String tipoUsuario);
+
+}
---
+++ ProyectoSAGLAM-war/src/java/Controlador/ControlPrueba.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Controlador;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author ANDRES PINEDA
+ */
+@ManagedBean
+@SessionScoped
+public class ControlPrueba {
+
+    /**
+     * Creates a new instance of ControlPrueba
+     */
+    public ControlPrueba() {
+    }
+    
+}
